ede0e1b4d3e02ed3ab6c8c47e800f11f575ce9b1
hugosenari <hugosenari@gmail.com>
2022-03-22 00:39:00 -0300

    chore: re-index ../nimpkgs/v/vecio

--- master/flake.nix
+++ master/flake.nix
@@ -9,7 +9,7 @@
   
   inputs.src-vecio-master.flake = false;
   inputs.src-vecio-master.owner = "emekoi";
-  inputs.src-vecio-master.ref   = "refs/heads/master";
+  inputs.src-vecio-master.ref   = "master";
   inputs.src-vecio-master.repo  = "vecio.nim";
   inputs.src-vecio-master.type  = "github";
   
